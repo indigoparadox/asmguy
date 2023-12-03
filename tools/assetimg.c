3e1d99b1b1a86ef104af5d2f9a8ebea222678bcd
indigoparadox <indigo.repo@interfinitydynamics.info>
2023-12-03 11:03:25 -0500

    Rough attempt to convert bitmap to CGA bytes.

--- tools/assetimg.c
+++ tools/assetimg.c
@@ -1,20 +1,34 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 
 #define ERR_BADARGS 1
 #define ERR_BADFILE 2
+#define ERR_ALLOC 4
 
 /* = stdint for DOS = */
 
 typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
 #if __WORDSIZE == 64
+typedef int int32_t;
 typedef unsigned int uint32_t;
 #else
+typedef long int32_t;
 typedef unsigned long uint32_t;
 #endif
 
 /* = Color Table = */
+
+#define CGA_BLACK 0
+#define CGA_CYAN 1
+#define CGA_MAGENTA 2
+#define CGA_WHITE 3
+
+#define CGA_DITHER_BLACK 1
+#define CGA_DITHER_MAGENTA 2
+#define CGA_DITHER_WHITE 4
 
 const uint8_t gc_black = 0;
 const uint8_t gc_darkblue = 1;
@@ -33,21 +47,74 @@ const uint8_t gc_magenta = 13;
 const uint8_t gc_yellow = 14;
 const uint8_t gc_white = 15;
 
+const uint8_t gc_cga_color_table[] = {
+   /*  0 */ CGA_BLACK,
+   /*  1 */ CGA_CYAN,
+   /*  2 */ CGA_CYAN,
+   /*  3 */ CGA_CYAN,
+   /*  4 */ CGA_MAGENTA,
+   /*  5 */ CGA_MAGENTA,
+   /*  6 */ CGA_CYAN,
+   /*  7 */ CGA_WHITE,
+   /*  8 */ CGA_WHITE,
+   /*  9 */ CGA_CYAN,
+   /* 10 */ CGA_CYAN,
+   /* 11 */ CGA_CYAN,
+   /* 12 */ CGA_MAGENTA,
+   /* 13 */ CGA_MAGENTA,
+   /* 14 */ CGA_CYAN,
+   /* 15 */ CGA_WHITE
+};
+
+const uint8_t gc_cga_flags_table[] = {
+   /*  0 */ 0,
+   /*  1 */ CGA_DITHER_BLACK,
+   /*  2 */ CGA_DITHER_BLACK,
+   /*  3 */ 0,
+   /*  4 */ CGA_DITHER_BLACK,
+   /*  5 */ CGA_DITHER_BLACK,
+   /*  6 */ CGA_DITHER_MAGENTA,
+   /*  7 */ CGA_DITHER_BLACK,
+   /*  8 */ CGA_DITHER_BLACK,
+   /*  9 */ 0,
+   /* 10 */ 0,
+   /* 11 */ 0,
+   /* 12 */ 0,
+   /* 13 */ 0,
+   /* 14 */ CGA_DITHER_WHITE,
+   /* 15 */ 0
+};
+
 #define COLOR_TABLE_CONST( idx, lc, uc, r, g, b ) \
 
 /* = Program = */
 
 int main( int argc, char* argv[] ) {
    int retval = 0;
+   int i = 0;
    FILE* bmp_file = NULL;
    uint32_t bmp_offset = 0;
    uint32_t bmp_type = 0;
    uint32_t bmp_sz = 0;
-   uint32_t bmp_linew = 0;
+   int32_t bmp_line_w = 0;
+   int32_t bmp_lines = 0;
    uint32_t iter_pos = 0;
    uint8_t bmp_px = 0;
+   uint32_t iter_line = 0;
+   uint32_t cga_plane_sz = 0;
+   uint8_t* cga_bytes_plane_1 = NULL;
+   uint32_t cga_byte_idx_plane_1 = 0;
+   uint8_t cga_bit_idx_plane_1 = 0;
+   uint8_t* cga_bytes_plane_2 = NULL;
+   uint32_t cga_byte_idx_plane_2 = 0;
+   uint8_t cga_bit_idx_plane_2 = 0;
+   uint8_t* cga_plane_bytes_sel = NULL;
+   uint32_t* cga_plane_byte_idx_sel = NULL;
+   uint8_t* cga_plane_bit_idx_sel = NULL;
 
+   assert( sizeof( int32_t ) == 4 );
    assert( sizeof( uint32_t ) == 4 );
+   assert( sizeof( uint16_t ) == 2 );
    assert( sizeof( uint8_t ) == 1 );
 
    if( 2 > argc ) {
@@ -83,7 +150,21 @@ int main( int argc, char* argv[] ) {
 
    /* Get bitmap line width. */
    fseek( bmp_file, 0x12, SEEK_SET );
-   fread( &bmp_linew, 4, 1, bmp_file );
+   fread( &bmp_line_w, 4, 1, bmp_file );
+   if( 0 == bmp_line_w ) {
+      printf( "bad line width (%d): %s\n", bmp_line_w, argv[1] );
+      retval = ERR_BADFILE;
+      goto cleanup;
+   }
+
+   /* Get bitmap height. */
+   fseek( bmp_file, 0x16, SEEK_SET );
+   fread( &bmp_lines, 4, 1, bmp_file );
+   if( 0 == bmp_lines ) {
+      printf( "bad line count (%d): %s\n", bmp_lines, argv[1] );
+      retval = ERR_BADFILE;
+      goto cleanup;
+   }
 
    /* Verify bitmap pixel size. */
    fseek( bmp_file, 0x1c, SEEK_SET );
@@ -100,6 +181,21 @@ int main( int argc, char* argv[] ) {
    if( 0 != bmp_type ) {
       printf( "bad compression (0x%02x): %s\n", bmp_type, argv[1] );
       retval = ERR_BADFILE;
+      goto cleanup;
+   }
+
+   cga_plane_sz = ((bmp_lines * bmp_line_w) / 4) / 2;
+   printf( "using CGA plane size: %d\n", cga_plane_sz );
+   cga_bytes_plane_1 = calloc( cga_plane_sz, 1 );
+   if( NULL == cga_bytes_plane_1 ) {
+      printf( "allocation error: CGA plane 1\n" );
+      retval = ERR_ALLOC;
+      goto cleanup;
+   }
+   cga_bytes_plane_2 = calloc( cga_plane_sz, 1 );
+   if( NULL == cga_bytes_plane_2 ) {
+      printf( "allocation error: CGA plane 1\n" );
+      retval = ERR_ALLOC;
       goto cleanup;
    }
 
@@ -120,15 +216,46 @@ int main( int argc, char* argv[] ) {
          printf( "0x%02x ", bmp_px );
       }
 
-      if( 0 == iter_pos % bmp_linew ) {
+      if( 0 == iter_pos % bmp_line_w ) {
          printf( "\n" );
       }
 #endif /* DEBUG_BMP */
 
       /* TODO: Output CGA bytes. */
+      if( 0 == bmp_lines % 2 ) {
+         /* Even plane. */
+         cga_plane_bytes_sel = cga_bytes_plane_1;
+         cga_plane_byte_idx_sel = &cga_byte_idx_plane_1;
+         cga_plane_bit_idx_sel = &cga_bit_idx_plane_1;
+      } else {
+         /* Odd plane. */
+         cga_plane_bytes_sel = cga_bytes_plane_2;
+         cga_plane_byte_idx_sel = &cga_byte_idx_plane_2;
+         cga_plane_bit_idx_sel = &cga_bit_idx_plane_2;
+      }
 
+      cga_plane_bytes_sel[*cga_plane_byte_idx_sel] <<= 2;
+      cga_plane_bytes_sel[*cga_plane_byte_idx_sel] |= 
+         gc_cga_color_table[bmp_px];
+
+      /* Figure out the next byte/bit. */
+      (*cga_plane_bit_idx_sel) += 2;
+      if( *cga_plane_bit_idx_sel >= 8 ) {
+         *cga_plane_bit_idx_sel = 0;
+         (*cga_plane_byte_idx_sel)++;
+      }
+
+      /* Next pixel. */
       iter_pos++;
+      if( 0 == iter_pos % bmp_line_w ) {
+         iter_line++;
+      }
    }
+
+   for( i = 0 ; cga_plane_sz > i ; i++ ) {
+      printf( "0%02xh, ", cga_bytes_plane_1[i] );
+   }
+   printf( "\n" );
 
 #ifdef DEBUG_BMP
    printf( "\n" );
